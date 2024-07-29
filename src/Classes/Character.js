8d73e7a3cb15227d82b803c80420b3e710bcc500
Lotfi <lotfi.djeg@gmail.com>
2024-07-30 00:21:14 +0200

    testListener

--- src/Classes/Character.js
+++ src/Classes/Character.js
@@ -23,28 +23,42 @@ class Character {
 
     displayCharacter() {
         const character = document.createElement("div");
-        const elADiv = document.createElement("a");
-        elADiv.href = '#';
-        elADiv.addEventListener('click', () => {console.log(`rediriger vers le detail du personnage`)});
-        elADiv.innerHTML = `
-        <a href="#" class="btn btn-primary">
-        <div class="custom-card" style="width: 18rem;">
-        <img src="${this.image}" class="card-img-top custom-image" alt="${this.name}">
-        <div class="card-body">
-        <h5 class="card-title">${this.name}</h5>
-        <p class="card-text">${this.race}-${this.gender}</p>
-        <p class="card-text">${this.ki}</p>
-        <p class="card-text">${this.maxKi}</p>
-        <p class="card-text">${this.affiliation}-${this.gender}</p>
-        </div>
-        </div>
+        character.classList.add("character-container");
+
+        character.innerHTML = `
+        <a href="#" class="btn btn-primary details">
+            <div class="custom-card" style="width: 18rem;">
+                <img src="${this.image}" class="card-img-top custom-image" alt="${this.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${this.name}</h5>
+                    <p class="card-text">${this.race} - ${this.gender}</p>
+                    <p class="card-text">Ki: ${this.ki}</p>
+                    <p class="card-text">Max Ki: ${this.maxKi}</p>
+                    <p class="card-text">Affiliation: ${this.affiliation}</p>
+                    <p class="card-text on">${this.description}</p>
+
+                </div>
+                </div>
         </a>
         `;
 
-        elADiv.character.appendChild();
+
 
         return character;
     }
+
+
+    
 }
 
 export default Character;
+
+// Ajouter un gestionnaire d'événement pour l'élément <a>
+const elADiv = document.querySelector("details");
+elADiv.addEventListener('click', (event) => {
+    event.preventDefault();
+    const description = document.querySelector(".on");
+    description.style.display="block";
+    
+    
+});
